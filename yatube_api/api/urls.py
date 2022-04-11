9dbad0cfb841cebf6bf797a2de95afac7cd1fe2c
Искров Антон <iskrov.95@mail.ru>
2022-04-11 09:50:47 +0300

    correct

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -7,7 +7,7 @@ from .views import CommentViewSet, GroupViewSet, PostViewSet
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'groups', GroupViewSet)
-router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet)
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='Comment')
 
 
 urlpatterns = [
